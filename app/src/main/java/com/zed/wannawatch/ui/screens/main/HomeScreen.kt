b54efd1b7fa0c837f7d9a82711bc71df93e2eec0
Zed <emailzed03@gmail.com>
2023-02-05 15:37:52 +1100

    implemented filtering dropdowns, lazy vertical grid and FAB button. navigation has not been implemented yet

--- app/src/main/java/com/zed/wannawatch/ui/screens/main/HomeScreen.kt
+++ app/src/main/java/com/zed/wannawatch/ui/screens/main/HomeScreen.kt
@@ -1,31 +1,33 @@
 package com.zed.wannawatch.ui.screens.main
 
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement.Absolute.SpaceBetween
+import androidx.compose.foundation.layout.Arrangement.SpaceEvenly
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowDropDown
 import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-
-enum class HomeScreenWatchedFilter(val watched: Boolean? = null) {
-    All, Watched(true), Unwatched(false)
-}
-
-enum class MovieRatingFilter(val ratingValue: Int) {
-    All(-1),
-    One(1),
-    Two(2),
-    Three(3),
-    Four(4),
-    Five(5)
-}
+import coil.compose.AsyncImage
+import com.zed.wannawatch.R
+import com.zed.wannawatch.services.HomeScreenWatchedFilter
+import com.zed.wannawatch.services.MovieRatingFilter
 
 
 @Composable
@@ -37,101 +39,150 @@ fun HomeScreen(viewModel: MainViewModel) {
     val watchedFilterOptions = listOf<HomeScreenWatchedFilter>(HomeScreenWatchedFilter.All, HomeScreenWatchedFilter.Watched, HomeScreenWatchedFilter.Unwatched)
     var watchedFilterExpanded by remember { mutableStateOf(false) }
 
-    var ratingfilterValue by remember { mutableStateOf(MovieRatingFilter.All) }
+    var ratingFilterValue by remember { mutableStateOf(MovieRatingFilter.All) }
     val ratingFilterOptions = listOf<MovieRatingFilter>(MovieRatingFilter.All, MovieRatingFilter.One, MovieRatingFilter.Two, MovieRatingFilter.Three, MovieRatingFilter.Four, MovieRatingFilter.Five)
     var ratingFilterExpanded by remember { mutableStateOf(false) }
 
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        // filter rows
-        Row(modifier = Modifier.fillMaxWidth()) {
-
-            // filter movies by watched/unwatched status
-            Box(){
-                Button(onClick = { watchedFilterExpanded = true }) {
-                    Text(text = "filter watched")
-                }
-
-                DropdownMenu(
-                    expanded = watchedFilterExpanded,
-                    onDismissRequest = { watchedFilterExpanded = false }) {
-                    watchedFilterOptions.forEach {
-                        DropdownMenuItem(
-                            onClick = {
-                                watchedFilterStatus = it
-                                watchedFilterExpanded = false
-                            },
-                            enabled = (it != watchedFilterStatus),
-                            text = {
-                                Row(horizontalArrangement = SpaceBetween) {
-                                    Text(it.toString())
-                                    Icon(Icons.Rounded.Check, contentDescription = "check mark for filter selected status")
-                                }
-                        })
-                    }
-                }
-            }
-
-            // filter movies by rating
-            Box(){
-                Button(onClick = { ratingFilterExpanded = true }) {
-                    Text(text = "filter by rating")
-                }
-
-                DropdownMenu(
-                    expanded = ratingFilterExpanded,
-                    onDismissRequest = { ratingFilterExpanded = false }) {
-                    ratingFilterOptions.forEach {
-                        DropdownMenuItem(
-                            onClick = {
-                                ratingfilterValue = it
-                                ratingFilterExpanded = false
-                            },
-                            enabled = (it != ratingfilterValue),
-                            text = {
-                                Row(horizontalArrangement = SpaceBetween) {
-                                    Text(it.toString())
-                                    if(it == ratingfilterValue) {
-                                        Icon(
-                                            Icons.Rounded.Check,
-                                            contentDescription = "check mark for rating selected status"
-                                        )
-                                    }
-                                }
-                            })
-                    }
-                }
-            }
-        }
-        //
-
-        if(movies?.isEmpty() == true) {
-            Text(text = "No movies added", textAlign = TextAlign.Center, modifier = Modifier.fillMaxSize())
-        } else {
-
-            val filteredMovies = movies!!.filter {
-                // filter by watched status
-                var result = false
-
-                result = if(watchedFilterStatus == HomeScreenWatchedFilter.All)
-                    true
-                else
-                    watchedFilterStatus.watched == it.watched
-
-                result
-            }.filter {
-                it.rating >= ratingfilterValue.ratingValue
-            }
-
-            LazyVerticalGrid(
-                columns = GridCells.Adaptive(minSize = 128.dp)
+    Box () {
+        Column(modifier = Modifier.fillMaxSize()) {
+            // filter options
+            // todo: export to component
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
             ) {
-                items(filteredMovies.size) {
-                    
+
+                // filter movies by watched/unwatched status
+                Box(){
+                    Button(
+                        border = BorderStroke(1.5.dp, Color.Black),
+                        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Black),
+                        onClick = {
+                            watchedFilterExpanded = true
+                        }
+                    ) {
+                        Text(text = "filter watched")
+                        Icon(Icons.Rounded.ArrowDropDown, contentDescription = "drop down arrow")
+                    }
+
+                    DropdownMenu(
+                        expanded = watchedFilterExpanded,
+                        onDismissRequest = { watchedFilterExpanded = false }) {
+                        watchedFilterOptions.forEach {
+                            DropdownMenuItem(
+                                onClick = {
+                                    watchedFilterStatus = it
+                                    watchedFilterExpanded = false
+                                },
+                                enabled = (it != watchedFilterStatus),
+                                text = {
+                                    Row(horizontalArrangement = SpaceBetween) {
+                                        Text(it.toString())
+                                        if(it == watchedFilterStatus)
+                                            Icon(Icons.Rounded.Check, contentDescription = "check mark for filter selected status")
+                                    }
+                                })
+                        }
+                    }
+                }
+
+                // filter movies by rating
+                Box(){
+                    Button(
+                        border = BorderStroke(1.5.dp, Color.Black),
+                        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Black),
+                        onClick = {
+                            ratingFilterExpanded = true
+                        }
+                    ) {
+                        Text(text = "filter by rating")
+                        Icon(Icons.Rounded.ArrowDropDown, contentDescription = "drop down arrow")
+                    }
+
+                    DropdownMenu(
+                        expanded = ratingFilterExpanded,
+                        onDismissRequest = { ratingFilterExpanded = false }) {
+                        ratingFilterOptions.forEach {
+                            DropdownMenuItem(
+                                onClick = {
+                                    ratingFilterValue = it
+                                    ratingFilterExpanded = false
+                                },
+                                enabled = (it != ratingFilterValue),
+                                text = {
+                                    Row(horizontalArrangement = SpaceEvenly) {
+                                        Text(it.toString())
+                                        if(it == ratingFilterValue) {
+                                            Icon(
+                                                Icons.Rounded.Check,
+                                                contentDescription = "check mark for rating selected status"
+                                            )
+                                        }
+                                    }
+                                })
+                        }
+                    }
                 }
             }
 
+            //
+            if(movies?.isEmpty() == true) {
+                Text(text = "No movies added", textAlign = TextAlign.Center, modifier = Modifier.fillMaxSize())
+            } else {
+
+                val filteredMovies = viewModel.filterMovies(movies ?: listOf(), ratingFilterValue, watchedFilterStatus)
+
+                LazyVerticalGrid(
+                    modifier = Modifier.fillMaxSize(),
+                    columns = GridCells.Adaptive(minSize = 128.dp)
+                ) {
+                    items(filteredMovies.size) {
+                        Box(modifier = Modifier.width(130.dp)) {
+
+                            AsyncImage(model = filteredMovies[it].posterUrl,
+                                contentDescription = "movie poster image",
+                                modifier = Modifier.width(128.dp)
+                            )
+
+                            if(filteredMovies[it].watched) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.wacthed_tick),
+                                    contentDescription = null,
+                                    modifier = Modifier.align(Alignment.TopEnd)
+                                )
+
+                            }
+
+                        }
+
+                    }
+                }
+
+            }
+
         }
+
+        SearchFAB(modifier = Modifier
+            .align(Alignment.BottomEnd)
+            .padding(20.dp)) {
+            Log.i("com.zed.wannawatch", "Search fab clicked")
+        }
+    }
+
+
+
+
+}
+
+@Composable
+fun SearchFAB(modifier: Modifier, onclick : () -> Unit) {
+    FloatingActionButton(
+        modifier = modifier,
+        onClick = onclick
+    ) {
+        Icon(Icons.Rounded.Search, contentDescription = "Search FAB")
     }
 
 }
