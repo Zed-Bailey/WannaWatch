9bcbf596b10ea6858f2706c38c008746378a765c
Zed <emailzed03@gmail.com>
2023-02-11 10:36:29 +1100

    added state variables and function to get detail model from api

--- app/src/main/java/com/zed/wannawatch/ui/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/zed/wannawatch/ui/screens/search/SearchViewModel.kt
@@ -1,11 +1,14 @@
 package com.zed.wannawatch.ui.screens.search
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.google.android.material.snackbar.Snackbar
+import com.zed.wannawatch.services.api.models.SearchDetail
 import com.zed.wannawatch.services.api.models.SearchResult
 import com.zed.wannawatch.services.models.Movie
 import com.zed.wannawatch.services.repository.ApiRepository
@@ -14,29 +17,57 @@ import kotlinx.coroutines.launch
 
 class SearchViewModel(private val repository: MovieRepository): ViewModel() {
 
-    val results = MutableLiveData<MutableList<SearchResult>>()
+    val results = MutableLiveData<List<SearchResult>>()
+
+
     private val apiRepository = ApiRepository()
-    val error = MutableLiveData<Boolean>()
+
+    var error by mutableStateOf(false)
+        private set
+
+    var loading by mutableStateOf(false)
+        private set
+
+    var detailResult = MutableLiveData<SearchDetail>()
+
 
     fun search(query: String) {
+        loading = true
         viewModelScope.launch {
             val result = apiRepository.searchRequest(query.trim())
             result.fold(onSuccess = { data ->
                 results.value = data.results
             }, onFailure = {
-                Log.e("SearchViewModel", "failed to execute network call :(")
-                error.value = true
+                Log.e("SearchViewModel", "failed to get search results :(")
+                error = true
             })
-
+            loading = false
         }
     }
+
+    fun getDetail(imdbId: String) {
+        viewModelScope.launch {
+            val result = apiRepository.searchDetail(imdbId)
+            result.fold(onSuccess = { data ->
+                detailResult.value = data
+            }, onFailure = {
+                Log.e("SearchViewModel", "failed to get detail :(")
+                error = true
+            })
+        }
+    }
+
+    fun clearDetail() {
+
+    }
+
 
     /**
      * if the error has been toggled to true, then this will toggle it off
      */
     fun toggleErrorOff() {
-        if(error.value == true) {
-            error.value = false
+        if(error == true) {
+            error = false
         }
     }
 
