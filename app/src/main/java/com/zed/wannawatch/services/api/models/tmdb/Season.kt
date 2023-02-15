2029737901f09314b04b7db71241f643042c073f
Zed <emailzed03@gmail.com>
2023-02-15 16:47:16 +1100

    fixed missing values in the json tmdb models

--- app/src/main/java/com/zed/wannawatch/services/api/models/tmdb/Season.kt
+++ app/src/main/java/com/zed/wannawatch/services/api/models/tmdb/Season.kt
@@ -2,11 +2,11 @@ package com.zed.wannawatch.services.api.models.tmdb
 import kotlinx.serialization.Serializable
 @Serializable
 data class Season(
-    val air_date: String,
+    val air_date: String?,
     val episode_count: Int,
     val id: Int,
     val name: String,
     val overview: String,
-    val poster_path: String,
+    val poster_path: String?,
     val season_number: Int
 )
--- app/src/main/java/com/zed/wannawatch/services/api/models/tmdb/TvExternalIds.kt
+++ app/src/main/java/com/zed/wannawatch/services/api/models/tmdb/TvExternalIds.kt
@@ -3,13 +3,13 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class TvExternalIds(
-    val facebook_id: String,
-    val freebase_id: String,
-    val freebase_mid: String,
+    val facebook_id: String?,
+    val freebase_id: String?,
+    val freebase_mid: String?,
     val id: Int,
-    val imdb_id: String,
-    val instagram_id: String,
-    val tvdb_id: Int,
-    val tvrage_id: Int,
-    val twitter_id: String
+    val imdb_id: String?,
+    val instagram_id: String?,
+    val tvdb_id: Int?,
+    val tvrage_id: Int?,
+    val twitter_id: String?
 )
