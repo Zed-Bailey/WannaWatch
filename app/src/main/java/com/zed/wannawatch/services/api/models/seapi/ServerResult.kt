4f058fbec5c9b5daf4bed2f3fd010a3cd1e93824
Zed <emailzed03@gmail.com>
2023-02-15 16:45:42 +1100

    server models for server embed

---
+++ app/src/main/java/com/zed/wannawatch/services/api/models/seapi/ServerResult.kt
@@ -0,0 +1,11 @@
+package com.zed.wannawatch.services.api.models.seapi
+
+@kotlinx.serialization.Serializable
+data class ServerResult(
+    val exact_match: Int,
+    val quality: String,
+    val server: String,
+    val size: Int,
+    val title: String,
+    val url: String
+)
