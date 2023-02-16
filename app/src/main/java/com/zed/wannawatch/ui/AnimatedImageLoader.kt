bb96e683ef42e83240cd53f45a81305401a5f084
Zed <emailzed03@gmail.com>
2023-02-16 14:10:24 +1100

    moved to a general location

--- app/src/main/java/com/zed/wannawatch/ui/AnimatedImageLoader.kt
+++ app/src/main/java/com/zed/wannawatch/ui/AnimatedImageLoader.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -13,12 +14,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
@@ -49,6 +50,7 @@ fun AnimatedImageLoader(url: String, width: Dp, height: Dp, contentDescription: 
             .size(coil.size.Size.ORIGINAL) // Set the target size to load the image at.
             .build(),
         onState = {
+            // update mutable values on state changes
             when (it) {
                 is AsyncImagePainter.State.Loading -> loading = true
                 is AsyncImagePainter.State.Success -> {
@@ -79,9 +81,8 @@ fun AnimatedImageLoader(url: String, width: Dp, height: Dp, contentDescription: 
                 .shimmer()
                 .background(MaterialTheme.colorScheme.surfaceVariant)
         ) { }
+
     } else if (success) {
-        val matrix = ColorMatrix()
-        matrix.setToSaturation(transition)
 
         Image(
             painter = painter,
@@ -92,18 +93,21 @@ fun AnimatedImageLoader(url: String, width: Dp, height: Dp, contentDescription: 
                 .scale(.8f + (.2f * transition))
                 .graphicsLayer { rotationX = (1f - transition) * 5f }
                 .alpha(min(1f, transition / .2f)),
-            colorFilter = ColorFilter.colorMatrix(matrix)
         )
+
     } else if (error) {
+
         Box(
             contentAlignment = Alignment.Center,
             modifier = Modifier
                 .width(width)
                 .height(height)
-                .background(MaterialTheme.colorScheme.surfaceVariant)
+                .background(MaterialTheme.colorScheme.surfaceVariant, RoundedCornerShape(10.dp))
+
         ) {
-            Text("Error loading image")
+            Text("Error loading image", textAlign = TextAlign.Center)
         }
+
     }
 
 }
