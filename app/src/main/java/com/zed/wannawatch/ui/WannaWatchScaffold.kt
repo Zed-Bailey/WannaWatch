d9275531504ecb49862abb4967bd5e8cf1f5ebe9
Zed <emailzed03@gmail.com>
2023-02-16 11:51:29 +1100

    scaffold now uses the appbar rather then my own row thing, more uniform across screens

--- app/src/main/java/com/zed/wannawatch/ui/WannaWatchScaffold.kt
+++ app/src/main/java/com/zed/wannawatch/ui/WannaWatchScaffold.kt
@@ -4,15 +4,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Delete
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 
 data class ScaffoldState(
     val shouldShowBack: Boolean = false,
@@ -21,6 +15,7 @@ data class ScaffoldState(
     val onDeletePressed: () -> Unit = {}
 )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WannaWatchScaffold(
     scaffoldState: ScaffoldState,
@@ -28,37 +23,34 @@ fun WannaWatchScaffold(
 ) {
 
     Column() {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 10.dp, vertical = 5.dp),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text("WannaWatch", style = MaterialTheme.typography.headlineSmall, fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.onBackground)
+            },
+            navigationIcon = {
+                if(scaffoldState.shouldShowBack) {
+                    IconButton(
+                        onClick = scaffoldState.onBackPressed,
+                    ) {
+                        Icon(Icons.Rounded.ArrowBack, contentDescription = null)
+                    }
+                }
+            },
 
-            if(scaffoldState.shouldShowBack) {
-                IconButton(
-                    onClick = scaffoldState.onBackPressed,
-                ) {
-                    Icon(Icons.Rounded.ArrowBack, contentDescription = null)
+            actions = {
+                if(scaffoldState.shouldShowDelete) {
+                    IconButton(
+                        onClick = scaffoldState.onDeletePressed,
+                    ) {
+                        Icon(Icons.Rounded.Delete, contentDescription = null)
+                    }
                 }
             }
 
-            Spacer(modifier = Modifier.weight(1f))
+        )
 
-            Text("WannaWatch", style = MaterialTheme.typography.headlineSmall, fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.onBackground)
+//        Spacer(Modifier.height(10.dp))
 
-            Spacer(modifier = Modifier.weight(1f))
-
-            if(scaffoldState.shouldShowDelete) {
-                IconButton(
-                    onClick = scaffoldState.onDeletePressed,
-                ) {
-                    Icon(Icons.Rounded.Delete, contentDescription = null)
-                }
-            }
-
-        }
-        Spacer(Modifier.height(10.dp))
         content()
 
     }
