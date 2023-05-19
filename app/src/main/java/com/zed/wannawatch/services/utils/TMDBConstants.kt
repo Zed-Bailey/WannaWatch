1f9f612ca58b721207b8fe012db39cb0869c8b07
Zed <emailzed03@gmail.com>
2023-05-19 15:14:47 +1000

    changed poster image size

--- app/src/main/java/com/zed/wannawatch/services/utils/TMDBConstants.kt
+++ app/src/main/java/com/zed/wannawatch/services/utils/TMDBConstants.kt
@@ -2,6 +2,6 @@ package com.zed.wannawatch.services.utils
 
 object TMDBConstants {
     const val imageBasePathOriginal = "https://image.tmdb.org/t/p/original/"
-    const val imageBasePath = "https://image.tmdb.org/t/p/w185/"
+    const val imageBasePath = "https://image.tmdb.org/t/p/w342/"
     const val backdropBasePath = "https://image.tmdb.org/t/p/w780/"
 }
