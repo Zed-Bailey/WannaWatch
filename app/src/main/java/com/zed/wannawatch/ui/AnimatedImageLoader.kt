60cd74e6f330adb6957201f7340e4891e077f1d3
Zed <emailzed03@gmail.com>
2023-05-22 11:58:20 +1000

    made error image load look nicer by just showing icon

--- app/src/main/java/com/zed/wannawatch/ui/AnimatedImageLoader.kt
+++ app/src/main/java/com/zed/wannawatch/ui/AnimatedImageLoader.kt
@@ -4,21 +4,27 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImagePainter
@@ -93,6 +99,7 @@ fun AnimatedImageLoader(url: String, width: Dp, height: Dp, contentDescription: 
             modifier = Modifier
                 .width(width)
                 .height(height)
+                .clip(RoundedCornerShape(5.dp))
                 .scale(.8f + (.2f * transition))
                 .graphicsLayer { rotationX = (1f - transition) * 5f }
                 .alpha(min(1f, transition / .2f)),
@@ -108,7 +115,7 @@ fun AnimatedImageLoader(url: String, width: Dp, height: Dp, contentDescription: 
                 .background(MaterialTheme.colorScheme.surfaceVariant, RoundedCornerShape(10.dp))
 
         ) {
-            Text("Error loading image", textAlign = TextAlign.Center)
+            Icon(Icons.Default.Image, "", tint = Color.White)
         }
 
     }
