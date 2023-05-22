947ec4677343415c4e424c6e7de9e5fc8e75577a
Zed <emailzed03@gmail.com>
2023-05-22 11:59:07 +1000

    redesigning the detail view to show more details. movie and tv shows will have different views

--- app/src/main/java/com/zed/wannawatch/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/zed/wannawatch/ui/screens/detail/DetailScreen.kt
@@ -3,87 +3,343 @@ package com.zed.wannawatch.ui.screens.detail
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.zed.wannawatch.R
-import com.zed.wannawatch.services.MovieApplication
 import com.zed.wannawatch.services.models.movie.Movie
+import com.zed.wannawatch.services.models.movie.MovieType
+import com.zed.wannawatch.services.utils.TMDBConstants
+import com.zed.wannawatch.ui.AppBarState
+import com.zed.wannawatch.ui.screens.BackDropImage
 import com.zed.wannawatch.ui.screens.search.AnimatedImageLoader
 
 @Composable
 fun DetailScreen(
     navController: NavController,
-    movieId: String,
-    viewModel: DetailViewModel = viewModel(
-        factory = DetailViewModelFactory(
-            repository = (LocalContext.current.applicationContext as MovieApplication).repository,
-            movieId = movieId
-        )
-    )
+    onComposing: (AppBarState) -> Unit,
+    viewModel: DetailViewModel = hiltViewModel()
 ) {
 
-    val movieState by viewModel.movieState.collectAsState()
+    val detailState by viewModel.movieState.collectAsState()
 
     LaunchedEffect(Unit) {
-        viewModel.getMovie(movieId)
+        viewModel.getMovie()
     }
 
-//    WannaWatchScaffold(
-//        scaffoldState = ScaffoldState(
-//            shouldShowBack = true,
-//            onBackPressed = {
-//                navController.navigateUp()
-//            },
-//            actions = {
-//                IconButton(
-//                    onClick = {
-//                        Log.i("com.zed.wannawatch", "delete pressed")
-//                        viewModel.delete(movieState)
-//                        navController.popBackStack()
-//                    }
-//                ) {
-//                    Icon(Icons.Rounded.Delete, contentDescription = null)
-//                }
-//            }
-//
-//        )
-//    ) {
-
-
-        movieState?.let {movie ->
-            Details(
-                movie = movie,
-                viewModel = viewModel,
-                onWatchClicked = {
-                    navController.navigate("watch_screen/${movie.imdbID}")
+    LaunchedEffect(key1 = true) {
+        onComposing(
+            AppBarState(
+                actions = {
+                    IconButton(onClick = {
+                        viewModel.delete(detailState)
+                        navController.popBackStack()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = null
+                        )
+                    }
+                },
+                navigation = {
+                    IconButton(onClick = {
+                        navController.navigateUp()
+                    }) {
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+                    }
                 }
             )
+        )
+    }
+
+    detailState?.let {movie ->
+        when(movie.resultType) {
+            MovieType.Movie -> {
+                MovieDetailScreen(movie = movie, viewModel = viewModel)
+            }
+
+            MovieType.Series -> {
+                TvDetailScreen(movie = movie, viewModel = viewModel)
+            }
         }
-
-//    }
-
-
+    }
 }
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Details(
+fun TvDetailScreen(
+    movie: Movie,
+    viewModel: DetailViewModel
+) {
+
+    val tvDetail by viewModel.tvDetailResult
+    val loading by viewModel.loading
+
+    LaunchedEffect(Unit) {
+        viewModel.getTvDetails()
+    }
+
+    if (loading) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Center) {
+            CircularProgressIndicator()
+        }
+    } else if (tvDetail == null) {
+
+        BasicDetailView(movie, viewModel)
+
+    } else {
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+
+
+            BackDropImage(
+                url = TMDBConstants.backdropBasePath + tvDetail?.backdrop_path,
+                title = movie.title
+            ) {
+
+                Button(
+                    modifier = Modifier.padding(end = 5.dp),
+                    onClick = { viewModel.toggleWatched() }) {
+                    Text(
+                        text = if (movie.watched) "UnWatch" else "Watched",
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 15.dp, top = 10.dp, 15.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    AnimatedImageLoader(url = movie.posterUrl, width = 128.dp, height = 170.dp)
+
+                    Spacer(modifier = Modifier.width(15.dp))
+
+                    Column(
+                        verticalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            "${tvDetail?.seasons?.size} seasons",
+                            style = MaterialTheme.typography.labelSmall
+                        )
+                        Text(
+                            "${tvDetail?.genresString}",
+                            style = MaterialTheme.typography.labelSmall
+                        )
+                        Text(
+                            "${tvDetail?.first_air_date}",
+                            style = MaterialTheme.typography.labelSmall
+                        )
+                    }
+                }
+
+
+                Text(tvDetail?.overview ?: "")
+
+                Spacer(modifier = Modifier.height(25.dp))
+
+                RatingRow(
+                    currRating = movie.rating,
+                    onClick = { ratingValue ->
+                        viewModel.updateRating(ratingValue)
+                    }
+                )
+
+
+                Spacer(modifier = Modifier.height(15.dp))
+
+                Text(
+                    "Series Notes",
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Left,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp, 0.dp)
+                )
+
+                OutlinedTextField(
+                    colors = TextFieldDefaults.outlinedTextFieldColors(MaterialTheme.colorScheme.onSurfaceVariant),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(128.dp)
+                        .padding(15.dp),
+                    value = movie.notes,
+                    onValueChange = {
+                        viewModel.updateNotesText(it)
+                    },
+                    maxLines = 10,
+                    placeholder = {
+                        Text(text = "Write any notes here")
+                    }
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+            }
+        }
+    }
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovieDetailScreen(
+    movie: Movie,
+    viewModel: DetailViewModel
+) {
+
+    val movieDetail by viewModel.movieDetailResult
+    val loading by viewModel.loading
+
+    LaunchedEffect(Unit) {
+        viewModel.getMovieDetails()
+    }
+
+    if(loading) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Center) {
+            CircularProgressIndicator()
+        }
+    } else if(movieDetail == null) {
+
+        BasicDetailView(movie, viewModel)
+
+    } else {
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+
+
+            BackDropImage(url = TMDBConstants.backdropBasePath + movieDetail?.backdrop_path, title = movie.title) {
+
+                Button(
+                    modifier = Modifier.padding(end = 5.dp),
+                    onClick = { viewModel.toggleWatched() }) {
+                    Text(
+                        text = if (movie.watched) "UnWatch" else "Watched",
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 15.dp, top = 10.dp, 15.dp)
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Start
+                    ) {
+                        AnimatedImageLoader(url = movie.posterUrl, width = 128.dp, height = 170.dp)
+
+                        Spacer(modifier = Modifier.width(15.dp))
+
+                        Column(
+                            verticalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text("${movieDetail?.runtime}", style = MaterialTheme.typography.labelSmall)
+                            Text(
+                                "${movieDetail?.genres?.forEach { it.name + ' ' }}",
+                                style = MaterialTheme.typography.labelSmall
+                            )
+                            Text(
+                                "${movieDetail?.release_date}",
+                                style = MaterialTheme.typography.labelSmall
+                            )
+                        }
+                    }
+
+
+                    Text(movieDetail?.overview ?: "")
+
+
+                    RatingRow(
+                        currRating = movie.rating,
+                        onClick = { ratingValue ->
+                            viewModel.updateRating(ratingValue)
+                        }
+                    )
+
+
+                    Spacer(modifier = Modifier.height(25.dp))
+
+                    Text(
+                        "Movie Notes",
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        fontSize = 20.sp,
+                        textAlign = TextAlign.Left,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp, 0.dp)
+                    )
+
+                    OutlinedTextField(
+                        colors = TextFieldDefaults.outlinedTextFieldColors(MaterialTheme.colorScheme.onSurfaceVariant),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(128.dp)
+                            .padding(15.dp),
+                        value = movie.notes,
+                        onValueChange = {
+                            viewModel.updateNotesText(it)
+                        },
+                        maxLines = 10,
+                        placeholder = {
+                            Text(text = "Write any notes here")
+                        }
+                    )
+
+                    Spacer(modifier = Modifier.height(10.dp))
+                }
+            }
+
+
+        }
+    }
+}
+
+
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BasicDetailView(
     movie: Movie,
     viewModel: DetailViewModel,
-    onWatchClicked : () -> Unit
 ) {
     Column(
         modifier = Modifier
@@ -123,21 +379,6 @@ fun Details(
                 fontWeight = FontWeight.Bold
             )
         }
-
-//        FilledTonalButton(
-//            shape = RoundedCornerShape(10.dp),
-//            onClick = {
-//                onWatchClicked()
-//            },
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .padding(40.dp, 5.dp)
-//                .align(CenterHorizontally),
-//            colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondary)
-//
-//        ) {
-//            Text(text = "Watch Online", color = MaterialTheme.colorScheme.onSecondary)
-//        }
 
         Spacer(Modifier.height(25.dp))
 
--- app/src/main/java/com/zed/wannawatch/ui/screens/detail/DetailViewModel.kt
+++ app/src/main/java/com/zed/wannawatch/ui/screens/detail/DetailViewModel.kt
@@ -1,19 +1,126 @@
 package com.zed.wannawatch.ui.screens.detail
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.zed.wannawatch.services.models.movie.Movie
+import com.zed.wannawatch.services.models.movie.MovieType
+import com.zed.wannawatch.services.models.tmdb.MovieDetailResult
+import com.zed.wannawatch.services.models.tmdb.TvDetailResult
 import com.zed.wannawatch.services.repository.MovieRepository
+import com.zed.wannawatch.services.repository.TMDBRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DetailViewModel(private val repository: MovieRepository, movieId: String): ViewModel() {
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val repository: MovieRepository,
+    private val tmdbRepository: TMDBRepository,
+
+    // navigation args are inject through savedstate handler
+    private val savedStateHandle: SavedStateHandle
+): ViewModel() {
 
     private val _movieState = MutableStateFlow<Movie?>(null)
     val movieState = _movieState.asStateFlow()
+
+    val movieDetailResult = mutableStateOf<MovieDetailResult?>(null)
+    val tvDetailResult = mutableStateOf<TvDetailResult?>(null)
+
+    val loading = mutableStateOf(true)
+
+    /**
+     * updates the models tmdb id
+     */
+    private fun updateTmdbId(imdbId: String, modelType: MovieType) = viewModelScope.launch {
+        val results = tmdbRepository.findTmdbIdFromImdbId(imdbId)
+        println(results)
+        results?.let { idResults ->
+
+            when(modelType) {
+                MovieType.Movie -> {
+                    // check that the item exists
+                    if(idResults.movie_results.any()) {
+                        // update state
+                        _movieState.update {
+                            it?.copy(
+                                tmdbId = idResults.movie_results.first().id
+                            )
+                        }
+                        println("found tmdb id of: ${idResults.movie_results.first().id} for imdb id: $imdbId")
+                    }
+
+                }
+
+                MovieType.Series -> {
+                    // check that the item exists
+                    if(idResults.tv_results.any()) {
+                        // update state
+                        _movieState.update {
+                            it?.copy(
+                                tmdbId = idResults.tv_results.first().id
+                            )
+                        }
+                    }
+                }
+            }
+
+            // update model in database
+            update(_movieState.value)
+
+        }
+    }
+
+
+
+    fun getMovieDetails() {
+        _movieState.value?.let { mov ->
+            viewModelScope.launch {
+
+                // 0 is default tmdbid
+                if(mov.tmdbId == 0) {
+                    println("id was 0, updating...")
+                    updateTmdbId(mov.imdbID, mov.resultType)
+                }
+
+                val detail = tmdbRepository.getMovieDetail(mov.tmdbId)
+                if(detail != null) {
+                    movieDetailResult.value = detail
+                }
+
+                loading.value = false
+            }
+        }
+
+    }
+
+
+
+    fun getTvDetails() {
+        _movieState.value?.let { mov ->
+            viewModelScope.launch {
+                // 0 is default tmdbid
+                if(mov.tmdbId == 0) {
+                    println("id was 0, updating...")
+                    updateTmdbId(mov.imdbID, mov.resultType)
+                }
+
+                val detail = tmdbRepository.getTvDetail(mov.tmdbId)
+                if(detail != null) {
+                    tvDetailResult.value = detail
+                }
+
+                loading.value = false
+            }
+        }
+
+    }
 
 
     fun toggleWatched() {
@@ -46,13 +153,17 @@ class DetailViewModel(private val repository: MovieRepository, movieId: String):
         update(_movieState.value)
     }
 
-    fun getMovie(id: String) = viewModelScope.launch {
-        val movie = repository.getMovie(id)
-        movie.collect { flow ->
-            _movieState.update {
-                flow
+    fun getMovie() = viewModelScope.launch {
+        val id = savedStateHandle.get<String>("movieId")
+        id?.let {
+            val movie = repository.getMovie(it)
+            movie.collect { flow ->
+                _movieState.update {
+                    flow
+                }
             }
         }
+
     }
 
     private fun update(movie: Movie?) = viewModelScope.launch{
@@ -73,15 +184,5 @@ class DetailViewModel(private val repository: MovieRepository, movieId: String):
 
 
 
-    }
-}
-
-class DetailViewModelFactory(private val repository: MovieRepository, private val movieId: String) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return DetailViewModel(repository, movieId) as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
