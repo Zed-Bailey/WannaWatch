80dd79dad45b90fc0692b30ea8b235672bcb8932
Zed <emailzed03@gmail.com>
2023-05-25 12:23:46 +1000

    error snackbar. added series view

--- app/src/main/java/com/zed/wannawatch/ui/screens/discoverDetail/DiscoverDetailScreen.kt
+++ app/src/main/java/com/zed/wannawatch/ui/screens/discoverDetail/DiscoverDetailScreen.kt
@@ -1,16 +1,17 @@
 package com.zed.wannawatch.ui.screens.discoverDetail
 
-import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -21,20 +22,40 @@ import com.zed.wannawatch.services.models.movie.MovieType
 import com.zed.wannawatch.services.utils.TMDBConstants
 import com.zed.wannawatch.ui.ErrorState
 import com.zed.wannawatch.ui.screens.search.MovieDetailDialog
+import com.zed.wannawatch.ui.screens.search.SeriesDetailDialog
+import kotlinx.coroutines.launch
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @Composable
 fun DiscoverDetailScreen(
+    snackbarHostState: SnackbarHostState,
     viewModel: DiscoverDetailViewModel = hiltViewModel()
 ) {
 
     val loading by viewModel.loading
 
     val movieDetail by viewModel.movieDetail.observeAsState()
+    val seriesDetail by viewModel.seriesDetail.observeAsState()
 
     val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
 
     LaunchedEffect(Unit) {
         viewModel.getDetail()
+    }
+
+
+    val errorState by viewModel.error
+    LaunchedEffect(errorState) {
+        when (val e = errorState) {
+            is ErrorState.Error -> {
+                snackbarHostState.showSnackbar(e.msg)
+            }
+
+            else -> {}
+        }
     }
 
     Box(
@@ -46,20 +67,21 @@ fun DiscoverDetailScreen(
             ),
         contentAlignment = Alignment.Center
     ) {
-        if(loading) {
+        if (loading) {
             LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
         } else {
 
-            when(viewModel.type!!) {
+            when (viewModel.type!!) {
                 MovieType.Movie -> {
-                    MovieDetailDialog(model = viewModel.movieDetail.value!!) {
-                        // todo
-                        movieDetail?.let {
+                    movieDetail?.let {
+                        MovieDetailDialog(model = it) {
+
                             val movie = Movie(
                                 imdbID = it.imdb_id!!,
                                 tmdbId = it.id,
                                 title = it.title,
-                                description = it.overview ?: it.tagline ?: "No description provided",
+                                description = it.overview ?: it.tagline
+                                ?: "No description provided",
                                 year = it.release_date.take(4).toInt(),
                                 resultType = MovieType.Movie,
                                 posterUrl = if (it.poster_path == null) "" else TMDBConstants.imageBasePath + it.poster_path
@@ -67,22 +89,44 @@ fun DiscoverDetailScreen(
 
                             viewModel.addModel(movie)
 
-                            Toast.makeText(context, "Added Movie", Toast.LENGTH_SHORT).show()
+                            coroutineScope.launch {
+                                snackbarHostState.showSnackbar("Added Movie", withDismissAction = true)
+                            }
+
                         }
 
                     }
                 }
 
-                MovieType.Series -> {
 
+                MovieType.Series -> {
+                    seriesDetail?.let {
+                        SeriesDetailDialog(
+                            model = it,
+                            onAdd = {
+                                val movie = Movie(
+                                    imdbID = "",
+                                    tmdbId = it.id,
+                                    title = it.name,
+                                    description = it.overview,
+                                    year = it.first_air_date.take(4).toInt(),
+                                    resultType = MovieType.Series,
+                                    posterUrl = TMDBConstants.imageBasePath + (it.poster_path
+                                        ?: it.backdrop_path),
+                                    seriesSeasons = Json.encodeToString(value = it.seasons),
+                                )
+                                viewModel.addModel(movie)
+
+                                coroutineScope.launch {
+                                    snackbarHostState.showSnackbar("Added Series", withDismissAction = true)
+                                }
+
+                            },
+                        )
+                    }
                 }
             }
         }
-
-        if(viewModel.error.value is ErrorState.Error) {
-
-        }
-
-
     }
 }
+
