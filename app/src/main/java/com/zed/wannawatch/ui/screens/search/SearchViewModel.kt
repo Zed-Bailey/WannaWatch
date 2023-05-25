0e5503c54c99331cac23449ba43f1b6bd4560bbb
Zed <emailzed03@gmail.com>
2023-05-25 12:21:46 +1000

    removed unused functions. added errorstate

--- app/src/main/java/com/zed/wannawatch/ui/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/zed/wannawatch/ui/screens/search/SearchViewModel.kt
@@ -1,21 +1,18 @@
 package com.zed.wannawatch.ui.screens.search
 
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.zed.wannawatch.services.models.movie.Movie
 import com.zed.wannawatch.services.models.movie.MovieType
-import com.zed.wannawatch.services.models.tmdb.MovieDetailResult
 import com.zed.wannawatch.services.models.tmdb.MovieResult
 import com.zed.wannawatch.services.models.tmdb.TMDBSearchResult
-import com.zed.wannawatch.services.models.tmdb.TvDetailResult
 import com.zed.wannawatch.services.models.tmdb.TvResult
 import com.zed.wannawatch.services.repository.MovieRepository
 import com.zed.wannawatch.services.repository.TMDBRepository
+import com.zed.wannawatch.ui.ErrorState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -30,20 +27,16 @@ class SearchViewModel @Inject constructor(
     val seriesSearchResults = MutableLiveData<TMDBSearchResult<TvResult>>()
 
 
-    var error by mutableStateOf(false)
-        private set
-
-    var errorMessage by mutableStateOf("")
-        private set
+    var errorState by mutableStateOf<ErrorState>(ErrorState.NoError)
 
     var loading by mutableStateOf(false)
         private set
 
-    var seriesDetail = MutableLiveData<TvDetailResult>()
-    var movieDetail = MutableLiveData<MovieDetailResult>()
+    var selected = mutableStateOf(MovieType.Movie)
 
-    var detailLoading = MutableLiveData(false)
-
+    fun setSelectedType(type: MovieType) {
+        selected.value = type
+    }
 
 
 
@@ -59,6 +52,7 @@ class SearchViewModel @Inject constructor(
                         movieSearchResults.value = response
                     } else {
                         // set error
+                        errorState = ErrorState.Error("Error loading movie search results")
                     }
 
                     loading = false
@@ -70,6 +64,7 @@ class SearchViewModel @Inject constructor(
                         seriesSearchResults.value = response
                     } else {
                         // set error
+                        errorState = ErrorState.Error("Error loading series search results")
                     }
 
                     loading = false
@@ -79,64 +74,10 @@ class SearchViewModel @Inject constructor(
 
     }
 
-    /**
-     * Gets the detail model for the movie or tv show from the api
-     */
-    fun getDetail(tmdbId: Int, type: MovieType) {
-        detailLoading.value = true
-        viewModelScope.launch{
-            when(type) {
-                MovieType.Movie -> {
+    fun retry(query: String, type: MovieType) {
 
-                    val response = tmdbRepository.getMovieDetail(tmdbId)
-                    if(response != null) {
-                        movieDetail.value = response
-                    } else {
-                        // set error
-                    }
-                    detailLoading.value = false
+        errorState = ErrorState.NoError
 
-                }
-
-                MovieType.Series -> {
-                    val response = tmdbRepository.getTvDetail(tmdbId)
-                    if(response != null) {
-                        seriesDetail.value = response
-                    } else {
-                        // set error
-                    }
-
-                    detailLoading.value = false
-                }
-            }
-        }
-    }
-
-
-    /**
-     * adds the movie model to the database
-     * if the movie is a series then it will also query the api and fetch the imdb id
-     */
-    fun addModel(movie: Movie) = viewModelScope.launch {
-        var model = movie
-
-        // if is a series fetch the imdb id from the api
-        if(movie.resultType == MovieType.Series) {
-            val response = tmdbRepository.getTvId(movie.tmdbId)
-            Log.d("com.zed.wannawatch", "$response")
-            if(response?.imdb_id != null) {
-                model = movie.copy(
-                    imdbID = response.imdb_id
-                )
-            } else {
-                error = true
-                errorMessage = "Failed to get IMDB id"
-            }
-
-        }
-
-        if(!error) {
-            repository.insertMovie(model)
-        }
+        search(query, type)
     }
 }
